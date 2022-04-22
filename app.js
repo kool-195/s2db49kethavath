c2d5072b80fee45ebced0dbec807f352757d26c9
kool-195 <muralikrishna8496@gmail.com>
2022-04-22 10:13:26 -0500

    kool

--- app.js
+++ app.js
@@ -3,42 +3,110 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('dotenv').config()
 
-const connectionString =  process.env.MONGO_CON  || "mongodb+srv://demo:demo@cluster0.laqt8.mongodb.net/demo?retryWrites=true&w=majority"
-mongoose = require('mongoose'); 
-console.log(connectionString)
-mongoose.connect(connectionString,  
-{useNewUrlParser: true, 
-useUnifiedTopology: true}); 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+    if (err) { return done(err); }
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    if (!user.validPassword(password)) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, user);
+    });
+  }
+));
+
+const connectionString =
+process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString,
+{useNewUrlParser: true,
+useUnifiedTopology: true});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var watchRouter = require('./routes/watch');
-var addmodsRouter = require('./routes/addmods');
-var selectorRouter = require('./routes/selector');
-var resourceRouter = require('./routes/resource');
-var watch = require("./models/watch");
-
+var addmodsRouter=require('./routes/addmods');
+var selectorRouter=require('./routes/selector');
+var watch = require('./models/watch');
+var resourceRouter=require('./routes/resource');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+// We can seed the collection if needed on server start
+async function recreateDB(){
+ // Delete everything
+ await watch.deleteMany();
+
+ let instance1 = new watch({watch_name:"Titan",  watch_color:'Black', watch_cost:1245}); 
+  instance1.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("First object saved") 
+  }); 
+
+  let instance2 = new watch({watch_name:"Gshock",  watch_color:'White', watch_cost:6789}); 
+  instance2.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("Second object saved") 
+  }); 
+
+  let instance3 = new watch({watch_name:"Fossil",  watch_color:'Blue', watch_cost:2349}); 
+  instance3.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("Third object saved") 
+  }); 
+
+}
+
+// List of all Costumes
+
+
+let reseed = true;
+if (reseed) { recreateDB();}
+
+// We can seed the collection if needed on server start
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
-app.use('/watch', watchRouter);
-app.use('/addmods', addmodsRouter);
-app.use('/selector', selectorRouter);
-app.use('/resource', resourceRouter);
+app.use('/watch',watchRouter);
+app.use('/addmods',addmodsRouter);
+app.use('/selector',selectorRouter);
+app.use('/resource',resourceRouter);
+
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,31 +124,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
 module.exports = app;
-
-// We can seed the collection if needed on server start 
-async function recreateDB(){ 
-  // Delete everything 
-  await watch.deleteMany(); 
- 
-  let instance1 = new watch({watch_name:"Titan",  watch_color:'Black', watch_cost:1245}); 
-  instance1.save( function(err,doc) { 
-      if(err) return console.error(err); 
-      console.log("First object saved") 
-  }); 
-
-  let instance2 = new watch({watch_name:"Gshock",  watch_color:'White', watch_cost:6789}); 
-  instance2.save( function(err,doc) { 
-      if(err) return console.error(err); 
-      console.log("Second object saved") 
-  }); 
-
-  let instance3 = new watch({watch_name:"Fossil",  watch_color:'Blue', watch_cost:2349}); 
-  instance3.save( function(err,doc) { 
-      if(err) return console.error(err); 
-      console.log("Third object saved") 
-  }); 
-} 
- 
-let reseed = true; 
-if (reseed) { recreateDB();}
